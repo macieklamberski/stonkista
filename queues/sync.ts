295de033bca04057bc4a6d8334bcfe4d65828c4e
Maciej Lamberski <maciej@lamberski.com>
2025-11-30 17:27:09 +0100

    chore: Update queue settings

--- queues/sync.ts
+++ queues/sync.ts
@@ -17,13 +17,11 @@ export const syncQueue = createQueue(
     },
     worker: {
       concurrency: 1,
-      lockDuration: 600000, // 10 min for sync job.
+      lockDuration: 1200000, // 20 min for sync job.
     },
   },
 )
 
-// Sync CoinGecko coins daily at 00:00.
-syncQueue.add('syncCoingecko', {}, { repeat: { pattern: '0 0 * * *' } })
-
-// Sync currencies weekly on Sunday at 00:00.
-syncQueue.add('syncFrankfurter', {}, { repeat: { pattern: '0 0 * * 0' } })
+// Sync weekly at 00:00.
+syncQueue.add('syncCoingecko', undefined, { repeat: { pattern: '0 0 * * 0' } })
+syncQueue.add('syncFrankfurter', undefined, { repeat: { pattern: '0 0 * * 0' } })
--- queues/tickers.ts
+++ queues/tickers.ts
@@ -3,4 +3,5 @@ import { createQueue } from '../utils/queues.ts'
 
 export const tickersQueue = createQueue('tickers', { scrapeTickers })
 
+// Sync daily at 00:05.
 tickersQueue.add('scrapeTickers', undefined, { repeat: { pattern: '5 0 * * *' } })
