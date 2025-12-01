19aba143a78f5de1ebe4e41a141c65859f396245
Maciej Lamberski <maciej@lamberski.com>
2025-12-01 11:52:22 +0100

    feat: Add trimming trailing slash when present in the URL

--- src/instances/hono.ts
+++ src/instances/hono.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { trimTrailingSlash } from 'hono/trailing-slash'
 import { basePath } from '../constants/bullboard.ts'
 import { routeHandler as bullboardRoutes } from '../instances/bullboard.ts'
 import { priceRoutes } from '../routes/price.ts'
 
 export const hono = new Hono()
+
+hono.use(trimTrailingSlash())
 
 hono.get('/', (context) =>
   context.redirect('https://github.com/macieklamberski/stonkista/blob/main/README.md'),
