04c5eef85c7187efe090557bf35a3dbde8cf706a
Maciej Lamberski <maciej@lamberski.com>
2025-12-13 12:56:09 +0100

    feat: Add ability to serve static files

--- src/instances/hono.ts
+++ src/instances/hono.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { basicAuth } from 'hono/basic-auth'
+import { serveStatic } from 'hono/bun'
 import { trimTrailingSlash } from 'hono/trailing-slash'
 import { basePath, password, username } from '../constants/bullboard.ts'
 import { routeHandler as bullboardRoutes } from '../instances/bullboard.ts'
@@ -10,6 +11,7 @@ import { forexRoutes } from '../routes/forex.ts'
 export const hono = new Hono()
 
 hono.use(trimTrailingSlash())
+hono.use('/static/*', serveStatic({ root: './src/' }))
 
 hono.get('/', (context) =>
   context.redirect('https://github.com/macieklamberski/stonkista/blob/main/README.md'),
