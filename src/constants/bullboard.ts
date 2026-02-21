d3cdcc345ed465e7b4502c48f6a67c07e0bc1a29
Maciej Lamberski <maciej@lamberski.com>
2026-02-21 12:55:49 +0100

    refactor: Extract Bull Board into separate Docker service

--- src/constants/bullboard.ts
+++
@@ -1,3 +0,0 @@
-export const basePath = '/bullboard'
-export const username = process.env.BULLBOARD_USER ?? ''
-export const password = process.env.BULLBOARD_PASS ?? ''
--- src/instances/hono.ts
+++ src/instances/hono.ts
@@ -1,9 +1,6 @@
 import { Hono } from 'hono'
-import { basicAuth } from 'hono/basic-auth'
 import { serveStatic } from 'hono/bun'
 import { trimTrailingSlash } from 'hono/trailing-slash'
-import { basePath, password, username } from '../constants/bullboard.ts'
-import { routeHandler as bullboardRoutes } from '../instances/bullboard.ts'
 import { cryptoRoutes } from '../routes/crypto.ts'
 import { equitiesRoutes } from '../routes/equities.ts'
 import { forexRoutes } from '../routes/forex.ts'
@@ -16,9 +13,6 @@ hono.use('/static/*', serveStatic({ root: './src/' }))
 hono.get('/', (context) =>
   context.redirect('https://github.com/macieklamberski/stonkista/blob/main/README.md'),
 )
-
-hono.use(`${basePath}/*`, basicAuth({ username, password }))
-hono.route(basePath, bullboardRoutes)
 
 hono.route('/forex', forexRoutes)
 hono.route('/crypto', cryptoRoutes)
