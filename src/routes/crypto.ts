4c2f2886bf7be5eef71b62eb911bc41f5bc160f8
Maciej Lamberski <maciej@lamberski.com>
2026-02-23 16:54:55 +0100

    feat: Return prices only in date range responses

--- src/routes/crypto.ts
+++ src/routes/crypto.ts
@@ -47,9 +47,7 @@ cryptoRoutes.get('/:ticker/:currencyOrDate?/:date?', async (context) => {
 
     context.header('Cache-Control', 'public, max-age=31536000')
 
-    const csv = entries
-      .map((entry) => `${entry.date},${formatPrice(entry.price, locale)}`)
-      .join('\n')
+    const csv = entries.map((entry) => formatPrice(entry.price, locale)).join('\n')
     return context.text(csv)
   }
 
