de691b9fe0d57cf32142e2c0162590ada6ed70fc
Maciej Lamberski <maciej@lamberski.com>
2026-03-01 13:19:12 +0100

    fix: Fix the forex rates fetching to only get EUR rates

--- src/jobs/scrapeCurrencies.ts
+++ src/jobs/scrapeCurrencies.ts
@@ -1,15 +1,6 @@
-import { tickers } from '../database/tables.ts'
-import { db } from '../instances/database.ts'
 import { frankfurterQueue } from '../queues/frankfurter.ts'
 
 export const scrapeCurrencies = async () => {
-  const allTickers = await db.select({ currency: tickers.currency }).from(tickers)
-  const currencies = [...new Set(allTickers.map((t) => t.currency))]
-
-  // No fromDate = fetch latest rates.
-  for (const currency of currencies) {
-    await frankfurterQueue.add('fetchFrankfurter', { baseCurrency: currency })
-  }
-
-  console.log(`[scrapeCurrencies] Scheduled ${currencies.length} Frankfurter jobs`)
+  await frankfurterQueue.add('fetchFrankfurter', { baseCurrency: 'EUR' })
+  console.log('[scrapeCurrencies] Scheduled Frankfurter job for EUR')
 }
